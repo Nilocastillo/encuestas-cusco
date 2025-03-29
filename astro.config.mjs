5e7a95809922d1b67a01b50b14c5bd64b21ed10c
Nilo <nilocastillloumeres@gmail.com>
2025-03-28 22:42:37 -0500

    actualización de cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Nilocastillo.github.io",
-  base: "cusco-encuestas",
+  base: "/encuestas-cusco", // Asegúrate de que la base esté correctamente configurada
   vite: {
     plugins: [tailwindcss()],
   },
